11a919cdbd03c577f65b3550a1583cebcbc1a1df
Nilton Mitsuharu Sugawara <nilton.sugawara@gmail.com>
2024-04-29 02:31:26 -0300

    itens resolvidos

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(fruit:i32) -> i32{
+    if fruit < 41 {
+        return fruit + fruit
+    }
+    fruit
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
